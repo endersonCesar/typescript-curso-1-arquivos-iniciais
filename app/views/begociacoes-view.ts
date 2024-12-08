52920613d82e9583448626c350b9c32b55bf136c
enderson.brufatto <enderson.brufatto@grupocard.com.br>
2024-12-08 17:16:29 -0400

    feat: criando tabela de interação

---
+++ app/views/begociacoes-view.ts
@@ -0,0 +1,16 @@
+export class NegociacoesView{
+    template():string{
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>QUANTIDADE</th>
+                    <th>VALOR</th>
+                </tr>
+            </thead>
+            
+        </table>
+        `
+    }
+}
